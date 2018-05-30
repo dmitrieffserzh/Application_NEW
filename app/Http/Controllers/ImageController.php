c3b73fc4e88eb8769e7efadcc9b4456070e82916
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-05-31 02:41:44 +0300

    update 0.1

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Profile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
+use Validator;
+
+class ImageController extends Controller {
+
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+
+    public function upload(Request $request, $x = NULL, $y = NULL) {
+
+        $id = Auth::id();
+
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:10000000',
+        ]);
+
+        if ($validator->passes()) {
+
+            $image = $request->file('image');
+            $input['image'] = time().'.'.$image->getClientOriginalExtension();
+
+            // THUMBNAILS
+            $destinationPath = public_path('uploads'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'thumbnails');
+            $img = Image::make($image->getRealPath());
+            // $img->crop(100, 100, 0, 0)->save($destinationPath.'/'.$input['image']);
+            $img->fit(100, 100)->save($destinationPath.DIRECTORY_SEPARATOR.$input['image']);
+
+            // NORMAL
+            $destinationPath = public_path('uploads'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'normals');
+            $img = Image::make($image->getRealPath());
+            $img->fit(1000, 1000)->save($destinationPath.DIRECTORY_SEPARATOR.$input['image']);
+
+            // COVER
+            $destinationPath = public_path('uploads'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'covers');
+            $img = Image::make($image->getRealPath());
+            $img->fit(1300, 700);
+            $img->blur(60)->save($destinationPath.DIRECTORY_SEPARATOR.$input['image']);
+
+            // ORIGINAL
+            $destinationPath = public_path('uploads'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'originals');
+            $image->move($destinationPath, $input['image']);
+
+            Profile::find($id)->update(['avatar'=> $input['image']]);
+
+            return response()->json(['success'=>'done', 'url'=> $input['image'] ]);
+        }
+
+        return response()->json(['error'=>$validator->errors()->all()]);
+   }
+
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -21,6 +21,8 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/components/modal.js', 'public/js/components')
     .js('resources/assets/js/components/modal-ajax.js', 'public/js/components')
     .js('resources/assets/js/components/rating.js', 'public/js/components')
+    .js('resources/assets/js/components/profile-image-uploader.js', 'public/js/components')
+    .js('resources/assets/js/components/main_menu.js', 'public/js/components')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/admin/admin.scss', 'public/css/admin')
     .sass('resources/assets/sass/admin/components/jq_minicolors.scss', 'public/css/admin/components');
