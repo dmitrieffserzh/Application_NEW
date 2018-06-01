f4cc60a1ded474935f81bdb3608ac0505026685f
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-06-01 16:39:04 +0300

    update

--- resources/views/story/partials/item.blade.php
+++ resources/views/story/partials/item.blade.php
@@ -16,11 +16,13 @@
                    title="{{ $post->owner->nickname }}">
                     {{ $post->owner->nickname }}
                 </a>
+
+                @if(Auth::user())
                 <span class="float-right">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#b6bcc7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
 
                 </span>
-
+                @endif
                 <span class="d-block text-muted small font-weight-light font-monospace">
                         {{ $post->created_at->diffForHumans() }}
                     </span>
