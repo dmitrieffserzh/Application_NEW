df195b017ece09d95b4c416d41875339138a6e59
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-06-05 18:15:17 +0300

    update

--- resources/views/components/users/user_menu_top.blade.php
+++ resources/views/components/users/user_menu_top.blade.php
@@ -28,6 +28,10 @@
 
 
 <div class="dropdown d-inline-block float-right">
+    @if(Auth::guest())
+        <a href="{{ route('login') }}" class="ajax-modal component-user-top__link" data-toggle="modal"
+        data-url="{{ route('login') }}" data-name="Войти" data-modal-size="modal-sm">Войти</a>
+    @else
     <a class="" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <img class="rounded-circle"
              style="width: 40px; height: 40px; border: 2px Solid rgba(255, 255, 255, 0.1);"
@@ -40,4 +44,5 @@
         <a class="k__link" href="#">Another action</a>
         <a class="k__link" href="#">Something else here</a>
     </div>
+    @endif
 </div>
